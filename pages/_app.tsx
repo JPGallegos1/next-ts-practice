535122a16b0a8e1decbb4ff5764517d363da9f74
Juan <jpgallegos20@gmail.com>
2021-04-19 17:13:54 -0300

    added: Chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
+import { StoreProvider } from "@context/Store";
 import theme from "../chakra.config";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
     </ChakraProvider>
   );
 }
